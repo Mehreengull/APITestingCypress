42c50f057544f995ebb2cd07b80fed07e3a36bf0
Mehreen Gull <gmehreen06@gmail.com>
2024-06-14 13:36:06 +0500

    Remove data

--- cypress.config.js
+++ cypress.config.js
@@ -4,20 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on("task", {
-        ReadDB({ dbConfig, sql }) {
-          const client = new pg.Pool(dbConfig);
-          return client.query(sql);
-        },
-      });
     },
     pageLoadTimeout: 120000,
-  },
-  DB: {
-    host: "numu-staging.cz2ayce8ak6w.eu-west-1.rds.amazonaws.com",
-    username: "mehreen_stage",
-    password: "5x8buWRHDb4g8kLFTZqa",
-    database: "stage_v5",
-    port: "5432",
   },
 });
